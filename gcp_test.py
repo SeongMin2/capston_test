2a37bddff7175630141ce231ea681cb986f577ea
SeongMin2 <soobark97@naver.com>
2022-05-23 21:48:27 +0900

    Modify auth key location

--- gcp_test.py
+++ gcp_test.py
@@ -2,9 +2,13 @@ import os
 from google.cloud import storage
 from ABS_PATH import ABS_PATH
 
-KEY_PATH = ABS_PATH + "/data/mlops-348504-1d12c4fc9b7d.json"
+KEY_PATH = ABS_PATH + "/gcp_auth_key/mlops-348504-1d12c4fc9b7d.json"
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = KEY_PATH
 
+# Donwload
 client = storage.Client(project="mlops-348504")
 bucket = client.bucket("sm_mlops_data")
+blob = bucket.blob("capston_data/text/train/beauty_health.json")
+
+blob.download_to_filename(ABS_PATH+"/test/beauty_health.json")

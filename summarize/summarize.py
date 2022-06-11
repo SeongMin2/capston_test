a98ba31c68e2538c84ef39ed73d97a6b0dd2ebeb
SeongMin2 <soobark97@naver.com>
2022-06-11 17:18:48 +0900

    mOidfy ;

--- summarize/summarize.py
+++ summarize/summarize.py
@@ -2,8 +2,8 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--a',type=int,default=1)
-    parser.add_argument('--b',type=int,default=2)
+    parser.add_argument('--a',type=str,default=1)
+    parser.add_argument('--b',type=str,default=2)
     args = parser.parse_args()
 
     c = args.a + args.b
